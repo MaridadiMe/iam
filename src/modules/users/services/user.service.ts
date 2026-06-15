0e319eec165c670ca36db80097d2935ee63f0d2e
crossroad <norbertseho@gmail.com>
2026-06-15 20:07:04 +0300

    Enabled OTPs

--- src/modules/users/services/user.service.ts
+++ src/modules/users/services/user.service.ts
@@ -104,7 +104,7 @@ export class UserService extends BaseService<User> {
         purpose: OtpPurpose.PHONE_VERIFICATION,
         userId: savedUser.id,
       };
-      //await this.requestOtp(otpDto);
+      await this.requestOtp(otpDto);
       return new UserResponseDto(savedUser);
     } catch (error) {
       this.logger.error(`Error While Saving A User: ${error}`);
@@ -148,7 +148,7 @@ export class UserService extends BaseService<User> {
       throw new BadRequestException('Phone already verified');
     }
 
-    //await this.otpService.verifyOtp(purpose, String(user.id), otp);
+    await this.otpService.verifyOtp(purpose, String(user.id), otp);
 
     user.isPhoneVerified = true;
     await this.userRepository.save(user);
